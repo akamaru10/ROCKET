3ae67cb84ffb7aa1786f4d733ae447a5c072678f
joão victor ferreira da silva <130696895+akamaru10@users.noreply.github.com>
2023-12-10 17:39:48 -0300

    at

--- dino_runner/components/power_ups/heart.py
+++
@@ -1,9 +0,0 @@
-from dino_runner.utils.constants import HEART, HEART_TYPE
-from dino_runner.components.power_ups.power_up import PowerUp
-
-
-class Heart(PowerUp):
-    def __init__(self):
-        self.image = HEART
-        self.type = HEART_TYPE
-        super().__init__(self.image, self.type)
